561eb33dfacbe3e75dc985b917fd2f4b2b891043
William Alex <william.alexwasm@gmail.com>
2025-01-24 21:43:27 -0300

    Create ex1036.c

---
+++ main/exercicios/ex1036.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main(){
+   double a, b, c, delta, x1, x2;
+
+   scanf("%lf %lf %lf",&A,&B,&C); 
+   delta = B * B - 4 * A * C; 
+   if(A==0 || delta<0){ 
+       printf("Impossivel calcular\n");
+   }
+   else{
+     x1 = (-B + sqrt(delta))/(2 * A); 
+     x2 = (-B - sqrt(delta))/(2 * A);
+     printf("R1 = %.5lf\n",x1);
+     printf("R2 = %.5lf\n",x2);
+   }
+   return 0;
+}
