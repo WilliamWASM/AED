127f1dcdc8fad73c587d87dc139a588170ddbc28
William Alex <william.alexwasm@gmail.com>
2025-02-09 17:32:33 -0300

    Update 1120.c

--- main/exercicios/1120.c
+++ main/exercicios/1120.c
@@ -1,33 +1,82 @@
 #include <stdio.h>
+#include <stdbool.h>
 #include <string.h>
 
-int main() {
-    char D, N[101];
-    
-    while (1) {
-        scanf(" %c %s", &D, N);
-        
-        if (D == '0' && N[0] == '0') {
+void retiraDefeito(char *, char);
+void retiraZeros(char *, char *);
+
+void main (){
+
+    unsigned short i, j;
+    char numero[300], numeroDefeituoso;
+    char numeroSemZero[300];
+
+    while (true)
+    {
+
+        scanf(" %c %s", &numeroDefeituoso, numero);
+
+        if (numeroDefeituoso == '0' && (strcmp(numero, "0") == 0))
             break;
-        }
-        
-        char result[101];
-        int j = 0;
-        
-        for (int i = 0; N[i] != '\0'; i++) {
-            if (N[i] != D) {
-                result[j++] = N[i];
-            }
-        }
-        
-        result[j] = '\0';
-        
-        if (j == 0 || (result[0] == '0' && result[1] == '\0')) {
+
+        retiraDefeito(numero, numeroDefeituoso);
+
+        retiraZeros(numero, numeroSemZero);
+
+        if (!strlen(numeroSemZero))
             printf("0\n");
-        } else {
-            printf("%s\n", result);
-        }
+        else
+            printf("%s\n", numeroSemZero);
+
+        memset(numero, 0, sizeof(numero));
+        memset(numeroSemZero, 0, sizeof(numeroSemZero));
+
     }
 
-    return 0;
+}
+
+void retiraDefeito(char *numero, char numeroDefeituoso){
+
+    unsigned short i;
+
+    i = 0;
+    while (numero[i])
+    {
+
+        if (numero[i] == numeroDefeituoso)
+            numero[i] = 'A';
+        i++;
+
+    }
+
+}
+
+void retiraZeros(char *numero, char *numeroSemZero){
+
+    unsigned short i, j;
+    char aux[300] = { 0 };
+
+    i = j = 0;
+    while (numero[i])
+    {
+
+        if (numero[i] != 'A')
+            aux[j++] = numero[i];
+
+        i++;
+    }
+
+    if (j == 0)
+        aux[j] = '0';
+
+    i = j = 0;
+    
+    while (aux[i] == '0')
+        i++;
+
+    while (aux[i])
+        numeroSemZero[j++] = aux[i++];
+
+    numeroSemZero[j] = '\0';
+
 }
